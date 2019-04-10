411fb1e7b6f76336863d79b8cfa7c7503b7a5a2e
vsp09 <vsp0915@gmail.com>
2019-04-10 11:50:53 +0530

    Q2.java

---
+++ Q2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Q2
+{
+	public static void main(String args[])
+	{
+	
+	int a;
+	
+	Scanner vsp=new Scanner(System.in);
+	System.out.print("Enter percentage: ");
+	a=vsp.nextInt();
+
+	if(a<35)
+		{
+			if(a>60)
+			   System.out.print("Grade A");
+			System.out.print("Grade B");
+		}
+		System.out.print("Grade C");
+	}
+
+}
